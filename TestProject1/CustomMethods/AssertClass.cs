026ca196da64164ab62c59cfac0b16b2d3e84eb9
Aditee <Aditee.belhekar@gmail.com>
2021-12-23 09:46:30 +0530

    new changes

--- TestProject1/CustomMethods/AssertClass.cs
+++ TestProject1/CustomMethods/AssertClass.cs
@@ -3,10 +3,61 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NUnit.Framework;
+using OpenQA.Selenium;
 
 namespace TestProject1.CustomMethods
 {
     internal class AssertClass
     {
+         public static bool IsElementPresent(IWebDriver driver,IWebElement element)
+        {
+            bool Present = false;
+            try
+            {
+                driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(60);
+                if (element.Displayed)
+                {
+                    Present = true;
+                    Console.WriteLine("Element Is Seen");
+                }
+
+                else
+                {
+                    driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(60);
+                    if (element.Displayed)
+                    {
+                        Present = true;
+                    }
+                }
+                return Present;
+            }
+            catch (NoSuchElementException ex)
+            {
+                Console.WriteLine("No Such Element : {0}", ex.Message);
+                Assert.Fail("No Such Element");
+                return false;
+            }
+        }
+
+        // Checks for Text and confirms its a match
+        public static void ContainsText(IWebElement element, string neededText)
+        {
+            string actualText = element.Text;
+            try
+            {
+                if (actualText.Contains(neededText))
+                {
+                    Console.WriteLine("Text Matched");
+                }
+                else
+                    Assert.Fail("Text mismatch");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Text Mismatched : {0}", e.Message);
+                throw e;
+            }
+        }
     }
 }
--- TestProject1/UnitTest1.cs
+++ TestProject1/UnitTest1.cs
@@ -2,7 +2,7 @@ using NUnit.Framework;
 
 namespace TestProject1
 {
-    public class Tests
+    public class Tests1
     {
         [SetUp]
         public void Setup()
