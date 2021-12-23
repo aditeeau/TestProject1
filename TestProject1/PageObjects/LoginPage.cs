683fa1d70286fbe638fe11be24795d3f64afdf6c
Aditee <Aditee@DESKTOP-27G4ILH>
2021-12-23 08:52:15 +0530

    Add project files.

---
+++ TestProject1/PageObjects/LoginPage.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace TestProject1.PageObjects
+{
+    internal class LoginPage
+    {
+        IWebDriver driver = new ChromeDriver();
+        
+    }
+}
